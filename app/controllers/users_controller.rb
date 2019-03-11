da886c44691b2776d0407f07ae5f8ca9e505acbf
luiszaba <luis.zabala076@gmail.com>
2019-03-11 14:56:08 -0400

    initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
 
 
+    get '/signup' do
+        if logged_in?
+          redirect to '/tweets'
+        else
+          erb :'users/create_user'
+      end
+    end
+
+    
 end
